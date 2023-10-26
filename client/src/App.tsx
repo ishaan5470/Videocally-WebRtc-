8797d09ae7d3dd8a3291cc3b5d687d81ebb8ee6a
ishaan5470 <ishaan8248@gmail.com>
2023-10-26 16:56:29 +0530

    intiated the socket connection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import socketIO from "socket.io-client";
+
+const WS= "http://localhost:8000";
 
 function App() {
+  
+  //in use Effect when our application is mounted we will connect to the server 
+  
+  useEffect(()=>{
+    socketIO(WS);
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <button>
+        start a meeting
+      </button>
     </div>
   );
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,31 @@
-console.log("it workdsssssssssss")
+import express from "express";
+import http from "http";
+// adding out websockets to this server
+import {Server, Socket} from "socket.io";
+
+// we cannot see user is connected because our both server is running at different port so to fix this
+import cors from "cors";
+
+const port=8000;
+const app=express(); // creating an express app
+app.use(cors)
+const server=http.createServer(app); // create a http server
+const io=new Server(server,{
+    cors:{
+        origin: "*",
+        methods:["GET","POST"],
+        },
+});
+
+io.on ("connection",(socket)=>{
+    console.log("user is connected")
+     //when the user shut downs the browser make a disconnnect event 
+     socket.on('disconnect',()=>{
+        console.log("user is disconnected");
+
+     })
+});
+
+server.listen(port, ()=>{
+    console.log(`i ammm listening on ${port}`)
+})
